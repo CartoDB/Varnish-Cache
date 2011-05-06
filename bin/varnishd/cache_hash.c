3bda6d772825e43f70541fa43c68e8c9f4ca3ebc
Poul-Henning Kamp <phk@FreeBSD.org>
2011-05-06 11:07:11 +0000

    Overhaul the ban_lurker to avoid a race condition where it checks an object at the same time a request-lookup does.
    The functional solution to this race is to hold the objhdr mutex,
    which we already hold briefly to get the refcount, also when we do
    the check.
    
    In terms of souce code this inlines the problematic HSH_FindBan()
    function in the lurker.
    
    And since that was major surgery af few other acts of improvement
    was carried out also.
    
    Most notably, we will now scan all applicable bans in the lurker
    and not give up on the first ban that tests req.* variables.
    
    Conflicts:
    	bin/varnishd/cache_ban.c
    	bin/varnishd/cache_hash.c

--- bin/varnishd/cache_hash.c
+++ bin/varnishd/cache_hash.c
@@ -669,42 +669,6 @@ HSH_DerefObjCore(struct sess *sp)
 		HSH_DeleteObjHead(sp->wrk, oh);
 }
 
-/*******************************************************************
- * This one is slightly tricky.  This is called from the BAN module
- * to try to wash the object which holds the oldest ban.
- * We compete against HSH_Deref() which comes in the opposite
- * locking order, we need to hold the BAN mutex, to stop the
- * BAN_DestroyObj() call in HSH_Deref(), so that the objhead
- * will not be removed under us.
- * NB: Do not call this function any other way or from any other
- * NB: place in the code.  It will not work for you.
- */
-
-void
-HSH_FindBan(struct sess *sp, struct objcore **oc)
-{
-	struct objcore *oc1, *oc2;
-	struct objhead *oh;
-
-	oc1 = *oc;
-	CHECK_OBJ_NOTNULL(oc1, OBJCORE_MAGIC);
-	oh = oc1->objhead;
-	CHECK_OBJ_NOTNULL(oh, OBJHEAD_MAGIC);
-	if (Lck_Trylock(&oh->mtx)) {
-		*oc = NULL;
-		return;
-	}
-	VTAILQ_FOREACH(oc2, &oh->objcs, list)
-		if (oc1 == oc2)
-			break;
-	if (oc2 != NULL && oc2->flags & OC_F_PERSISTENT)
-		SMP_Fixup(sp, oh, oc2);
-	if (oc2 != NULL)
-		oc2->refcnt++;
-	Lck_Unlock(&oh->mtx);
-	*oc = oc2;
-}
-
 void
 HSH_Deref(struct worker *w, struct object **oo)
 {
